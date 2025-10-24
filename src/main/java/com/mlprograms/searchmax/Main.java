56b14bb8d68e4583e58d97e0670e0f3ea4edaf72
Max Lemberg <max.lemberg2402@gmail.com>
2025-10-25 00:15:00 +0200

    Improve exception handling and use final variables in Main class initialization

--- src/main/java/com/mlprograms/searchmax/Main.java
+++ src/main/java/com/mlprograms/searchmax/Main.java
@@ -1,9 +1,28 @@
 package com.mlprograms.searchmax;
 
+import com.mlprograms.searchmax.controller.SearchController;
+import com.mlprograms.searchmax.model.SearchModel;
+import com.mlprograms.searchmax.service.SearchService;
+import com.mlprograms.searchmax.view.SearchView;
+
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Development has started. Seen developer branch for more details.");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (final Exception exception) {
+                System.err.println(exception.getMessage());
+            }
+
+            final SearchService searchService = new SearchService();
+            final SearchModel searchModel = new SearchModel();
+            final SearchController searchController = new SearchController(searchService, searchModel);
+            final SearchView searchView = new SearchView(searchController, searchModel);
+            searchView.setVisible(true);
+        });
     }
 
 }
