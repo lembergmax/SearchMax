a804657393371b6638dea9707c5e4971dcad85e5
Max Lemberg <max.lemberg2402@gmail.com>
2025-10-26 10:50:41 +0100

    initial commit

--- src/main/java/com/mlprograms/searchmax/Main.java
+++ src/main/java/com/mlprograms/searchmax/Main.java
@@ -1,26 +1,9 @@
 package com.mlprograms.searchmax;
 
-import com.mlprograms.searchmax.controller.SearchController;
-import com.mlprograms.searchmax.model.SearchModel;
-import com.mlprograms.searchmax.service.SearchService;
-import com.mlprograms.searchmax.view.SearchView;
-
-import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            try {
-                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            } catch (Exception ignored) {
-            }
-
-            SearchService service = new SearchService();
-            SearchModel model = new SearchModel();
-            SearchController controller = new SearchController(service, model);
-            SearchView view = new SearchView(controller, model);
-            view.setVisible(true);
-        });
+        System.out.println("Development has started. Seen developer branch for more details.");
     }
 
 }
--- src/main/java/com/mlprograms/searchmax/service/SearchEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.mlprograms.searchmax.service;
-
-public interface SearchEventListener {
-    void onId(String id);
-    void onMatch(String match);
-    void onEnd(String summary);
-    void onError(String message);
-}
-
